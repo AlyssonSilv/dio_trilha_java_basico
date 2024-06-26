688a550c20b9815521ed9fefc5f3601a4a839323
AlyssonSilv <alyssonsamueldasilva6@gmail.com>
2024-06-26 14:03:44 -0300

    projeto finalizado, com comentários, solicita normalmente o inserimento dos dados, e permite o inserimento de dados via Terminal.

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        //Objeto Scanner para leitura dos dados via terminal
+        Scanner scanner = new Scanner(System.in);
+
+        //leitura dos dados
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+        
+        // Consome a nova linha
+        scanner.nextLine();
+        
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
+        // Exibição da mensagem final
+        String mensagem = String.format("Olá %s, obrigado por criar uma conta conosco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+        System.out.println(mensagem);
+
+        // Fechamento do objeto Scanner
+        scanner.close();
     }
 }
